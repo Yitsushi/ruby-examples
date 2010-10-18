0eb1c221a6a0ae56237a3bf865b921e12faf24ca
Raito Yitsushi <yitsushi@gmail.com>
2010-10-18 15:48:31 +0200

    unit_testing: Example Shoulda unit test with a simple dynamical Rakefile funny: Some funny/amaze code... not requested (Just for Fun)

---
+++ funny/utf-magic.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+# def
+class Array; alias ⇈ join; end
+def ⇉(↯); puts ↯; end
+def ↬(*↯); Array.new(↯); end
+
+# var
+¤ = 0
+€ = 'E'
+ß = 'B'
+Đ = 'D'
+→ = ' -> '
+
+# The magic ^_^
+⇉ ↬(¤,€,ß,Đ).⇈ →
---
+++ unit_testing/test/tennis_scorer.shoulda.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+load 'lib/tennis_scorer.rb'
+#require 'lib/tennis_scorer.rb'
+
+class TennisScorerTest < Test::Unit::TestCase
+  def assert_score(target)
+    assert_equal(target, @ts.score)
+  end
+
+  context "Tennis Scores" do
+    setup do
+      @ts = TennisScorer.new
+    end
+
+    should "start with score of 0-0" do
+      assert_score('0-0')
+    end
+
+    context "where the server wins a point" do
+      setup do
+        @ts.give_point_to(:server)
+      end
+
+      should "be 15-0" do
+        assert_score('15-0')
+      end
+
+      context "and the oponent wins a point" do
+        setup do
+          @ts.give_point_to(:receiver)
+        end
+
+        should "be 15-15" do
+          assert_score('15-15')
+        end
+      end
+    end
+
+    should "be 0-15 if the receiver wins a point" do
+      @ts.give_point_to(:receiver)
+      assert_score('0-15')
+    end
+  end
+end
